3686cb8b319c5e697a46c16acd2b05c183853cb4
Emmanuel-Anaba <emmaanaba29@gmail.com>
2024-04-22 21:07:05 +0100

    Added Trig Functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 const App = () => {
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState(" ");
   const clearAll = () => {
     setResult("");
   };
@@ -12,14 +12,18 @@ const App = () => {
     setResult(String(result + e));
   };
   const calc = () => {
-    setResult(String(eval(result)));
-    localStorage.setItem("lastAns", eval(result));
+    if (eval(result) === undefined) {
+      alert("Syntax Error");
+    } else {
+      setResult(`${eval(result)}`);
+      localStorage.setItem("lastAns", eval(result));
+    }
   };
   const lastAns = () => {
     let item = localStorage.getItem("lastAns");
     setResult(String(result + item));
   };
-  const addPeriod = (e) => {
+  const addPeriodOrOperator = (e) => {
     !result.endsWith(".") &&
       !result.endsWith("+") &&
       !result.endsWith("-") &&
@@ -27,36 +31,58 @@ const App = () => {
       !result.endsWith("/") &&
       add(e);
   };
+  const sqr = (n) => {
+    n = eval(result);
+    n !== "" && setResult(String(n ** 2));
+  };
+  const sin = (n) => {
+    n = eval(result);
+    n *= (Math.PI / 180);
+    n = Math.sin(n);
+    n = Math.fround(n);
+    console.log(n);
+    // n !== "" && setResult(n);
+  };
+  const cos = (n) => {
+    n = eval(result);
+    n !== "" && setResult(String(n ** 2));
+  };
+  const tan = (n) => {
+    n = eval(result);
+    n !== "" && setResult(String(n ** 2));
+  };
   const Buttons = [
-    { text: "DEL", doSmth: goBack },
     { text: "AC", doSmth: clearAll },
+    { text: "DEL", doSmth: goBack },
     { text: "Ans", doSmth: lastAns },
-    { text: "/", doSmth: add },
+    { text: "sqr", doSmth: sqr },
+    { text: "sin", doSmth: sin },
+    { text: "cos", doSmth: cos },
+    { text: "tan", doSmth: tan },
+    { text: "/", doSmth: addPeriodOrOperator },
     { text: "8", doSmth: add },
     { text: "7", doSmth: add },
     { text: "9", doSmth: add },
-    { text: "*", doSmth: add },
+    { text: "*", doSmth: addPeriodOrOperator },
     { text: "4", doSmth: add },
     { text: "5", doSmth: add },
     { text: "6", doSmth: add },
-    { text: "-", doSmth: add },
+    { text: "-", doSmth: addPeriodOrOperator },
     { text: "1", doSmth: add },
     { text: "2", doSmth: add },
     { text: "3", doSmth: add },
-    { text: "+", doSmth: add },
+    { text: "+", doSmth: addPeriodOrOperator },
     { text: "0", doSmth: add },
     { text: "00", doSmth: add },
-    { text: ".", doSmth: addPeriod },
+    { text: ".", doSmth: addPeriodOrOperator },
     { text: "=", doSmth: calc },
   ];
 
   return (
     <>
       <main className="h-svh grid place-items-center text-emerald-100">
-        <div className="grid gap-2 bg-emerald-800 p-2 rounded">
-          <div className="bg-emerald-600 p-2 rounded">
-            <p>{result}</p>
-          </div>
+        <div className="grid gap-2 bg-emerald-800 p-2 rounded w-[80%] h-[80%]">
+          <div className="bg-emerald-600 rounded">{result}</div>
           <div className="grid grid-cols-4 gap-2">
             {Buttons.map(({ text, doSmth }) => (
               <button key={text} onClick={(e) => doSmth(e)}>
